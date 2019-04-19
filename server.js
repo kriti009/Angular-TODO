9682da465c2b63e5d3ce5c39f1353f4b99195f24
kriti009 <kriti.ritu.dew@gmail.com>
2019-04-19 11:27:38 +0530

    template parse error

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var express = require('express'),
     bodyParser = require('body-parser');
 var Todo = require('./models/todo');
 
-// Todo.create({name : "Apply for internship"},function(err){
+// Todo.create({name : "Complete internship task"},function(err){
+//     if(err){
+//         console.log(err);
+//     }
 //     console.log("created");
 // });
 app.use(bodyParser.urlencoded({extended: true}));
-mongoose.connect("mongodb://localhost:27017/Todo",{ useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/angular-todo",{ useNewUrlParser: true});
 app.get('/todo', function(req , res){
     Todo.find({}, function(err, data){
         if(err){
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todos/todo-list/todo-list.component';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +15,11 @@ import { TodoListComponent } from './todos/todo-list/todo-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
